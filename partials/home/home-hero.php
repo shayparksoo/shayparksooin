1fded10246630982ee023d377b062d2a6c937e79
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-02-28 15:18:13 -0800

    Remove testing code

--- partials/home/home-hero.php
+++ partials/home/home-hero.php
@@ -5,7 +5,7 @@
             <img class="home-hero__logo" src="../img/logo.svg" alt="Shay Park's Logo" width="260" height="260">
         </div>
         <div class="col col-12-sm col-6-md col-6 home-hero__name">
-            <h1>Shay Park!!</h1>
+            <h1>Shay Park</h1>
             <p class="subtitle">UI UX Designer | Customer-Centric | BCIT Student</p>
         </div>
     </div>
