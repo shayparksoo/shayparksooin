068a4d236379997ca2b630e9116018f549f9a2d6
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-03-07 20:02:37 -0800

    <b> added to artifacts

--- partials/uiux/circular/circular-purpose.php
+++ partials/uiux/circular/circular-purpose.php
@@ -2,7 +2,7 @@
     <div class="grid">
         <div class="sctn-box__title-cont col col-12-sm col-6-md col-6 artifact__title flex">
                 <h3>Purpose of the Project</h3>
-                <p class="card-body padding-49-r">The purpose was to <b>research the user experience</b> of online shopping and implying the information to create an online clothing shop using skills from Adobe XD and Figma. The design only includes the checkout process of a selected item for desktop version and sign-up process for app version.</p>
+                <p class="card-body padding-49-r">The purpose was to <b>research the user experience</b> of online shopping and implying the information to <b>create a prototype</b> of an online clothing shop using skills from Adobe XD and Figma. The design only includes the checkout process of a selected item for desktop version and sign-up process for app version.</p>
         </div>
         <div class="artifact__specs col-12-sm col-6-md col-6 flex border-l">
             <div class="artifact-specs__ind-text">
--- partials/uiux/onetap/onetap-purpose.php
+++ partials/uiux/onetap/onetap-purpose.php
@@ -2,7 +2,7 @@
     <div class="grid">
         <div class="sctn-box__title-cont col col-12-sm col-6-md col-6 artifact__title flex">
                 <h3 class="">Purpose of the Project</h3>
-                <p class="card-body card-purpose">The purpose of the project was to redesign the rough UI/UX of Point of System software to give better usability and learnability to the users. The objective was to modernize the design for users to find it more comfortable and attractive to use the product. The design needed to adapt the perspective of waiter/waitress of a restaurant who receives customers' menu and input it into the system.</p>
+                <p class="card-body card-purpose">The purpose of the project was to <b>redesign</b> the rough UI/UX of Point of System software to give <b>better usability</b> and <b>learnability</b> to the users. The objective was to modernize the design for users to find it more comfortable and attractive to use the product. The design needed to adapt the perspective of waiter/waitress of a restaurant who receives customers' menu and input it into the system.</p>
         </div>
         <div class="artifact__specs col col-12-sm col-6-md col-6 flex border-l">
             <div class="artifact-specs__ind-text">
