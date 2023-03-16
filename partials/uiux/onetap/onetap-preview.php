5c477a3e341f022367784f6bdf8e3cca8b98508e
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-03-16 15:11:12 -0700

    fix merge conflict

--- partials/uiux/onetap/onetap-preview.php
+++ partials/uiux/onetap/onetap-preview.php
@@ -1,11 +1,6 @@
 <section class="artifact-final grid border-b padding-top padding-bottom">
     <div class="sctn-box__title-cont col col-12 artifact-final__title flex img-center">
-<<<<<<< Updated upstream
         <img src="../img/onetap-preview.jpg" alt="Circular Mobile Version">
         <p class="caption flex padding-bottom">A Preview of One Tap Interface</p>
-=======
-        <img src="../img/onetap-preview.jpg" class="preview-img" alt="Circular Mobile Version">
-        <p class="caption flex">A Preview of One Tap Interface</p>
->>>>>>> Stashed changes
     </div>
 </section>
