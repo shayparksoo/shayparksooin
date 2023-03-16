356384b52cf9e7c3b907c43465892624465bb432
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-03-16 15:52:01 -0700

    Move development changes to main

---
+++ partials/uiux/vrra/vrra-purpose.php
@@ -0,0 +1,28 @@
+<section class="artifact-purpose border-b">
+    <div class="grid">
+        <div class="sctn-box__title-cont col col-12-sm col-6-md col-6 artifact__title flex">
+                <h3>Purpose of the Project</h3>
+                <p class="card-body padding-49-r">This project was assigned from UI UX 2 class from BCIT and the purpose was to perform a <b>study of user experience</b> of a website. The project included research of a potential website that can be redesigned, a development and production of user research, and analyzation of data result with a possible design improvement of wireframes.</p>
+                <p class="card-body padding-49-r p-break">The project was completed with 4 other members (Marlowe, Karla, Amanna & Michael). I participated performing user testing, producing UI/UX design of branding and high fidelity wireframes.</p>
+        </div>
+        <div class="artifact__specs col-12-sm col-6-md col-6 flex border-l">
+            <div class="artifact-specs__ind-text">
+                <h3>Date</h3>
+                <p class="card-body">Oct 12, 2022 - November 13, 2022</p>
+            </div>
+            <div class="artifact-specs__ind-text">
+                <h3>Role</h3>
+                <p class="card-body">Test Moderator</p>
+                <p class="card-body">UI/UX Designer</p>
+            </div>
+            <div class="artifact-specs__ind-text">
+                <h3>Software</h3>
+                <p class="card-body">Figma - Wireframes </p>
+                <p class="card-body">Google Forms - Screener Survery </p>
+                <p class="card-body">Zoom - User Testing/Recording </p>
+                <p class="card-body">System Usability Scale - Data Analysis </p>
+                
+            </div>
+        </div>
+    </div>
+</section>
