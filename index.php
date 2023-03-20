70d529e5f739bc38e36096bc4c238246d55de201
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-03-19 21:55:08 -0700

    test for og tag

--- index.php
+++ index.php
@@ -9,6 +9,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Shay Park's UI UX Design Porfolio Website">
         <meta name="keywords" content="shay park portfolio, soo in park portfolio, Portfolio, design, UI, UX, UI UX, UI UX Designer, UI UX Designer, graphic, graphic designer, photographer, figma, illustrator, photoshop, vancouver, bcit">
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://shayparksooin.com/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Shay Park's Portfolio Website">
+        <meta property="og:description" content="Welcome to Shay Park's portfolio website! Shay is a UI/UX designer who graduated from BCIT. Shay focuses to satisfy users' experience and goal by showcasing visual designs. Visit her website to see her prototype and connect with her through social media. ">
+        <meta property="og:image" content="https://imgur.com/GAF7X4O">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta property="twitter:domain" content="shayparksooin.com">
+        <meta property="twitter:url" content="https://shayparksooin.com/">
+        <meta name="twitter:title" content="Shay Park's Portfolio">
+        <meta name="twitter:description" content="Shay Park's UI UX Design Porfolio Website">
+        <meta name="twitter:image" content="https://imgur.com/5AOlFzL">
+
         <title>Shay Park's Portfolio</title>
         <link rel="icon" type="image/svg" href="/img/favicon.svg">
         <meta name="author" content="Shay Park, Soo In Park">
