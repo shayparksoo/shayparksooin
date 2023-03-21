0810f74c7dff58e74da68827a72f4280a4322a91
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-03-20 20:46:03 -0700

    alt tags added on buttons

--- partials/home/home-work.php
+++ partials/home/home-work.php
@@ -7,13 +7,13 @@
     <div class="work__cont grid">
         <div class="work__ind-card col col-12-sm col-6-md col-4 home-card">
             <div class="card-img">
-                <a href="<?php echo WWW_ROOT . '/uiux-circular.php';?>"><div class="card-img__circular card-img border-b" title="Circular UI UX by Shay Park"></div></a>
+                <a href="<?php echo WWW_ROOT . '/uiux-circular.php';?>"><div class="card-img__circular card-img border-b" title="Circular UI/UX by Shay Park"></div></a>
             </div>
             <div class="card-content">
                 <h3 class="card-h3">Circular - UX Design</h3>
                 <p class="card-category">UI/UX</p>
                 <p class="card-body">Researching the user experience of a checkout and registration process to improve user satisfaction and usability.</p>
-                <a href="<?php echo WWW_ROOT . '/uiux-circular.php';?>"><button class="btn btn-card-top btn-outline button-txt">View Project</button></a>
+                <a href="<?php echo WWW_ROOT . '/uiux-circular.php';?>" alt="View Circular Project"><button class="btn btn-card-top btn-outline button-txt">View Project</button></a>
             </div>
         </div>
         <div class="work__ind-card col col-12-sm col-6-md col-4 home-card">
@@ -24,18 +24,18 @@
                 <h3 class="card-h3">VRRA - UI/UX</h3>
                 <p class="card-category">UI/UX Design</p>
                 <p class="card-body">The project included plannning, executing, and analyzing the user experience of a website, along with a prototype that reflected the result.</p>
-                <a href="<?php echo WWW_ROOT . '/uiux-vrra.php';?>"><button class="btn btn-card-top btn-outline button-txt">View Project</button></a>
+                <a href="<?php echo WWW_ROOT . '/uiux-vrra.php';?>" alt="View VRRA Project"><button class="btn btn-card-top btn-outline button-txt">View Project</button></a>
             </div>
         </div>
         <div class="work__ind-card col col-12-sm col-6-md col-4 last-card">
             <div class="card-img">
-                <a href="<?php echo WWW_ROOT . '/uiux-onetap.php';?>"><div class="card-img__onetap card-img border-b" title="One Tap UI UX by Shay Park"></div></a>
+                <a href="<?php echo WWW_ROOT . '/uiux-onetap.php';?>"><div class="card-img__onetap card-img border-b" title="One Tap UI/UX by Shay Park"></div></a>
             </div>
             <div class="card-content">
                 <h3 class="card-h3">One Tap - UI Design</h3>
                 <p class="card-category">UI/UX</p>
                 <p class="card-body">A personal UI project to design a Point of Sales System to enhance the look of a software for an efficent task completion.</p>
-                <a href="<?php echo WWW_ROOT . '/uiux-onetap.php';?>"><button class="btn btn-card-top btn-outline button-txt">View Project</button></a>
+                <a href="<?php echo WWW_ROOT . '/uiux-onetap.php';?>" alt="View One Tap Project"><button class="btn btn-card-top btn-outline button-txt">View Project</button></a>
             </div>
         </div>
     </div>
