76e2b9f85429331b868c54a85a0935e40588c6fd
shayparksoo <103973831+shayparksoo@users.noreply.github.com>
2023-04-05 20:11:18 -0700

    copy changed for abouts

--- partials/about/about-bio.php
+++ partials/about/about-bio.php
@@ -4,9 +4,9 @@
             <img src="/img/profile.png" class="profile border-r" alt="Shay Park's Profile" width="300" height="194">
         </div>
         <div class="col col-12-sm col-7-md col-5 col-8-xl sec-body-ctnr about-bio-txt">
-            <p class="sec-body-sm p-break">I'm dedicated to producing a user-centric designs that satisfies the <b>users</b> and fulfill their needs. I love making designs that are simple, intuitive, and easy to use. I'm not afraid to listen to feedback that will make me <b>grow</b> and develop my skills.</p>
-            <p class="sec-body-sm p-break"> I came from Seoul to Vancouver in 2003. I'm very fortunate to have great <b>friends</b> and <b>community</b> who helped me to be who I am today. </p>
-            <p class="sec-body-sm p-break">Outside of work, I like to hand wash my car or go out for a photoshoot.</p>
+            <p class="sec-body-sm p-break">I'm dedicated to producing a user-centric designs that satisfies the <b>users</b> and builds trust between the brand and the users. For this reason, I build my designs to be intuitive, and easy to use. I'm open to receive feedbacks that will make me <b>grow</b> and develop my skills.</p>
+            <p class="sec-body-sm p-break"> I came from Seoul to Vancouver in 2003. It's been a long journey and therefore, I look for trust and stability in my relationships and what I do. I'm very fortunate to have great <b>friends</b> and <b>community</b> who helped me to be who I am today. And I seek to implement what I value in my design.</p>
+            <p class="sec-body-sm p-break">Outside of work, I like to hand wash my car or go out for a photoshoot with a nice walk.</p>
         </div>
     </div>
 </section>
